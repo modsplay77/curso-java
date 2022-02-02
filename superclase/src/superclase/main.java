4d727765420edf6fbb9ee9f2ee9433602b0ea68d
Modsplay co <38813040+modsplay77@users.noreply.github.com>
2022-02-02 18:38:03 +0100

    Add files via upload

---
+++ superclase/src/superclase/main.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package superclase;
+
+/**
+ * @author studi
+ *
+ */
+public class main {
+
+	/**
+	 * 
+	 */
+	public main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String ballena;
+		//ejemple de comparació de dades
+		//leon i ballena son variables
+	
+		if (leon.equals(ballena)) {
+			//accions
+			
+			//aquest metode mostra la clase a la que pertany l'objete @direccio fisica de memoria
+			System.out.println(variable.toString());
+		}
+
+	}
+
+}
---
+++ superclase/src/superclase/superclass.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package superclase;
+
+/**
+ * @author studi
+ *
+ */
+public class superclass {
+	//Atributos
+	int vida;
+	int energia;
+	
+
+	/**
+	 * 
+	 */
+	public superclass() {
+		// TODO Auto-generated constructor stub
+	vida = 100;
+	energia = 100;
+	
+	}
+	
+	//constructor SIEMPRE FUERA DE LOS ATRIBUTOS "CLASE()"
+	public void funcion() {
+		
+		vida = 100;
+		energia =100; 
+	}
+
+
+
+}
