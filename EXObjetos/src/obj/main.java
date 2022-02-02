e3d4c4721d8872572f9cf30d2d9cf9bb3135e0b8
Modsplay co <38813040+modsplay77@users.noreply.github.com>
2022-02-02 18:32:31 +0100

    Add files via upload

---
+++ EXObjetos/src/obj/main.java
@@ -0,0 +1,28 @@
+package obj;
+/**
+ * 
+ */
+
+/**
+ * @author studi
+ *
+ */
+public class main {
+
+	/**
+	 * 
+	 */
+	public main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		obj inglesa = new casas();
+		obj unifamiliar = new casas();
+	}
+
+}
---
+++ EXObjetos/src/obj/obj.java
@@ -0,0 +1,29 @@
+package obj;
+public class obj {
+	
+//Crear clase e instanciar 3 objetos. El objeto 2 será una copia del 1.
+	//realizar funcion comparacion entre objeto 1 y 2, salida por pantalla.
+	
+	//atributos globales
+	static String nombre;
+	static int cantidad;
+	static String tipo;
+	
+
+	public obj() {
+		// TODO Auto-generated constructor stub
+	//no inicializamos nada
+	}
+	//creamos el objeto1
+	public void casas{
+		tipo = "Construcción";
+	}
+	
+	
+	
+	public void coche(){
+		tipo = "Máquina";
+	}
+	 
+	
+}
